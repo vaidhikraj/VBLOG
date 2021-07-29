2c0f27d1190e8138ee7544d149dd2f03cbede9c4
vaidhikraj <t.vaidhikraj18@gmail.com>
2021-07-29 17:45:42 +0530

    comi

--- finalblog/settings.py
+++ finalblog/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
-    'whitenose.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 CORS_ALLOWED_ORIGINS = [
